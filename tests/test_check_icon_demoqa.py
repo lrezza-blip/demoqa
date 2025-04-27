8814acc3f2ba27b57a626edd6dfed50eaab7f12b
Valeriia Kenesbay <valeriiakenesbay@yandex.ru>
2025-04-27 14:36:21 +0300

    1 commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,21 +1,11 @@
 from pages.demoqa import DemoQa
-import time
 
 def test_check_icon(browser):
     demo_qa_page = DemoQa(browser)
-    time.sleep(3)
     demo_qa_page.visit()
-    demo_qa_page.click_on_the_icon()
-    time.sleep(3)
+    demo_qa_page.icon.click()
     assert demo_qa_page.equal_url()
-    assert demo_qa_page.exist_icon()
+    assert demo_qa_page.icon.exist()
 
-    # browser.get("https://demoqa.com/")
-    #
-    # icon = browser.find_element(By.CSS_SELECTOR,'header > a > img')
-    # if icon is None:
-    #     print('элемент не найден')
-    # else:
-    #     print('элемент найден')
 
 
